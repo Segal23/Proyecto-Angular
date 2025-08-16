f2de8b25703343a870257c4945e9967abbcf2040
Sebastián Gallegos <seba.gallegosu@gmail.com>
2025-08-16 13:38:28 -0300

    Navbar logic and template were changed to add 'Users' and 'Logout' options

--- src/features/navbar/navbar.ts
+++ src/features/navbar/navbar.ts
@@ -1,14 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AuthState } from '../../core/auth/auth.reducer';
+import { selectRole } from '../../core/auth/auth.selectors';
+import { AuthService } from '../../core/auth/auth-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrls: ['./navbar.css']
 })
-
-
 export class Navbar {
+  role$: Observable<string | null>;
 
+  constructor(
+    private store: Store<{ auth: AuthState }>,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.role$ = this.store.select(selectRole);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
