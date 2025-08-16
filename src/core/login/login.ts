d99e1fe3b37d7041b99c0468179650590114811b
Sebastián Gallegos <seba.gallegosu@gmail.com>
2025-08-16 13:18:16 -0300

    A login component was generated to allow user to login into the app

---
+++ src/core/login/login.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth/auth-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class Login {
+  loginForm: FormGroup;
+  submitted = false;
+  errorMsg = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)
+      ]]
+    });
+  }
+
+  // getter para usar en el HTML
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.errorMsg = '';
+  
+    if (this.loginForm.invalid) return;
+  
+    const { username, password } = this.loginForm.value;
+    try {
+      this.authService.login(username, password);
+      this.router.navigate(['/alumnos']);
+    } catch (err: any) {
+      this.errorMsg = err.message; 
+    }
+  }
+}
