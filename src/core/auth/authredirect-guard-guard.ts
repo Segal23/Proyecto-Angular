b30b5da965c1a33e225ec47680769c495b33423c
Sebastián Gallegos <seba.gallegosu@gmail.com>
2025-08-16 13:10:18 -0300

    Authredirect guard was created

---
+++ src/core/auth/authredirect-guard-guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, take, map } from 'rxjs';
+import { AuthState } from './auth.reducer';
+
+@Injectable({ providedIn: 'root' })
+export class AuthRedirectGuard implements CanActivate {
+  constructor(private store: Store<{ auth: AuthState }>, private router: Router) {}
+
+  canActivate(): Observable<boolean | UrlTree> {
+    return this.store.select('auth').pipe(
+      take(1),
+      map(auth => auth.user ? this.router.createUrlTree(['/alumnos']) : this.router.createUrlTree(['/login']))
+    );
+  }
+}
---
+++ src/core/auth/authredirect-guard.spec.ts
@@ -0,0 +1,17 @@
+// import { TestBed } from '@angular/core/testing';
+// import { CanActivateFn } from '@angular/router';
+
+// import { authredirectGuardGuard } from './authredirect-guard-guard';
+
+// describe('authredirectGuardGuard', () => {
+//   const executeGuard: CanActivateFn = (...guardParameters) => 
+//       TestBed.runInInjectionContext(() => authredirectGuardGuard(...guardParameters));
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//   });
+
+//   it('should be created', () => {
+//     expect(executeGuard).toBeTruthy();
+//   });
+// });
