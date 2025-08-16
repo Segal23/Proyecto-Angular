a2e685ad0cb6c6e1e6d13bfc4ef44f473fb28b62
Sebastián Gallegos <seba.gallegosu@gmail.com>
2025-08-16 13:39:58 -0300

    Toolbar logic and templated were changed to allow handle and store users

--- src/features/toolbar/toolbar.ts
+++ src/features/toolbar/toolbar.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { Bigtitle } from '../../shared/directives/bigtitle';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AuthState } from '../../core/auth/auth.reducer';
+import { selectAuth } from '../../core/auth/auth.selectors';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [Bigtitle],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './toolbar.html',
-  styleUrl: './toolbar.css'
+  styleUrls: ['./toolbar.css']
 })
 export class Toolbar {
+  auth$: Observable<AuthState>;
 
+  constructor(private store: Store<{ auth: AuthState }>) {
+    this.auth$ = this.store.select(selectAuth);
+  }
 }
